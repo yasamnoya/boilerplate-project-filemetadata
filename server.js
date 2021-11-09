10729079f2f46355969325eec69500a47d9353eb
yasamnoya <yasamnono@gmail.com>
2021-11-09 12:51:03 +0800

    feature: fileanalyse

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+const upload = multer({ dest: 'upload/' });
 require('dotenv').config()
 
 var app = express();
@@ -11,7 +13,14 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const file = req.file
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size,
+  });
+})
 
 
 const port = process.env.PORT || 3000;
